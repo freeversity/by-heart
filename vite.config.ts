8b5d11ec967b5ddeeac06b1efdb795237bbb92ea
Kyryl Stronko <kirstronko@gmail.com>
2025-07-05 11:28:29 -0400

    Init PR

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import react from '@vitejs/plugin-react-swc';
+import wyw from '@wyw-in-js/vite';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  build: {
+    outDir: './docs',
+  },
+  plugins: [
+    react(),
+    wyw({
+      include: ['**/*.{ts,tsx}'],
+      babelOptions: {
+        presets: ['@babel/preset-typescript', '@babel/preset-react'],
+      },
+    }),
+    checker({ typescript: true, biome: true }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './docs/index.html',
+          dest: './',
+          rename: '404.html',
+        },
+      ],
+    }),
+  ],
+});
